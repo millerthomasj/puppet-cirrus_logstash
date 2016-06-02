f1ff7573e4c224ef54ee5189277dd8886a820c96
Tom Miller <tmiller@twcable.com>
2016-06-02 10:10:16 -0600

    Adding basic puppet module config.

---
+++ manifests/config.pp
@@ -0,0 +1,17 @@
+class twc-logstash::config ()
+{
+  logstash::configfile { 'input_syslog':
+    source => "puppet:///modules/twc-logstash/input-syslog.conf",
+    order    => 2,
+  }
+
+  logstash::configfile { 'filter_apache':
+    source => "puppet:///modules/twc-logstash/filter-syslog.conf",
+    order  => 20,
+  }
+
+  logstash::configfile { 'output_es':
+    source => "puppet:///modules/twc-logstash/output-es.conf",
+    order   => 90,
+  }
+}
---
+++ manifests/install.pp
@@ -0,0 +1,10 @@
+class twc-logstash::install (
+  logstash_package_url = 'https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.3.2-1_all.deb',
+)
+{
+  class { 'logstash':
+    package_url => $logstash_package_url,
+    java_install => true,
+
+  include twc-logstash::config
+}
