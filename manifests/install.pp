b664acb721856a5d205cd3f3fa33d96741848672
Tom Miller <tmiller@twcable.com>
2016-06-02 10:14:34 -0600

    Adding logstash as an elasticsearch client.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,6 +2,8 @@ class twc-logstash::install (
   logstash_package_url = 'https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.3.2-1_all.deb',
 )
 {
+  include twc-elasticsearch::client
+
   class { 'logstash':
     package_url => $logstash_package_url,
     java_install => true,
