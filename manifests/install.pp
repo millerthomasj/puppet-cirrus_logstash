7f04bc1f3623be8ed1911855f3e92beda4f20c1e
Tom Miller <tmiller@twcable.com>
2016-06-14 10:39:13 -0600

    Adding ports for beats and syslog.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,7 @@
 class cirrus_logstash::install (
   $logstash_package_url = 'https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.3.2-1_all.deb',
-  $syslog_port = $cirrus_logstash::config::syslog_port,
-  $filebeat_port = $cirrus_logstash::config::filebeat_port,
-) inherits logstash::params {
+)
+{
   class { 'logstash':
     package_url => $logstash_package_url,
     java_install => true,
@@ -10,5 +9,5 @@ class cirrus_logstash::install (
 
   logstash::plugin { 'logstash-input-beats': }
 
-  include cirrus_logstash::config:
+  include cirrus_logstash::config
 }
--- manifests/params.pp
+++
@@ -1,5 +0,0 @@
-class cirrus_logstash::params ()
-{
-  $syslog_port = '5000',
-  $filebeat_port = '5044',
-}
