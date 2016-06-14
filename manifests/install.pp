c093a359e8e558b471aa2a31af3b5bd79ad03349
Tom Miller <tmiller@twcable.com>
2016-06-14 10:34:23 -0600

    trying to make this work.

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,8 +2,7 @@ class cirrus_logstash::install (
   $logstash_package_url = 'https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.3.2-1_all.deb',
   $syslog_port = $cirrus_logstash::config::syslog_port,
   $filebeat_port = $cirrus_logstash::config::filebeat_port,
-)
-{
+) inherits logstash::params {
   class { 'logstash':
     package_url => $logstash_package_url,
     java_install => true,
---
+++ manifests/params.pp
@@ -0,0 +1,5 @@
+class cirrus_logstash::params ()
+{
+  $syslog_port = '5000',
+  $filebeat_port = '5044',
+}
