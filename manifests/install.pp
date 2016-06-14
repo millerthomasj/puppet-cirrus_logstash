a812cf2d2ee0f676797cf15020987233c68b9183
Tom Miller <tmiller@twcable.com>
2016-06-14 10:13:39 -0600

    Fixing configuration.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,7 @@
 class cirrus_logstash::install (
   $logstash_package_url = 'https://download.elastic.co/logstash/logstash/packages/debian/logstash_2.3.2-1_all.deb',
-  $syslog_port = 5000,
-  $filebeat_port = 5044,
+  $syslog_port = '5000',
+  $filebeat_port = '5044',
 )
 {
   class { 'logstash':
