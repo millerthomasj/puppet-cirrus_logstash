fcaba8f6ea63f7ea4d92f1d69e0a7aeb7108c32e
Nathan Randall <nathan.randall@twc-contractor.com>
2016-06-17 16:12:14 -0600

    Fix create_resources for Filebeat Prospectors

--- manifests/client.pp
+++ manifests/client.pp
@@ -13,17 +13,20 @@
 #   Array of Logstash server '<hostname>:<port>' destinations to which Beats will ship events.
 #
 # [* cirrus_logstash::client::filebeat_prospectors *]
-#   Array of hashes, where each hash sets parameters for a Filebeat Prospector. A Prospector is
-#   a configuration element used by Filebeat to determine a set of paths from which to harvest
-#   logs. For every path specified in a Prospector, a Harvester process is started to read that
-#   log file (similar to "tail -f", but the default is to read the whole file and then follow as
-#   necessary).
-#   We use one Prospector per path (Harvester) to allow for greater granularity & flexibility
-#   further down the ELK data pipeline. Filebeat ONLY sets metadata at the Prospector (vice
-#   Harvester) level.
-#   In Hiera, specify filebeat_prospectors as an array of hashes. We merge the arrays from
-#   the entire hierarchy, then create a Prospector per hash. At a minimum, each hash should
-#   specify "paths" (array) and "fields" (hash).
+#   A hash of hashes, where each nested hash sets parameters for a Filebeat Prospector.
+#   A Prospector is a configuration element used by Filebeat to determine a set of paths from
+#   which to harvest logs. For every path specified in a Prospector, a Harvester process is
+#   started to read that log file (similar to "tail -f", but the default is to read the whole
+#   file and then follow as necessary).
+#
+#   Since Filebeat ONLY sets metadata at the Prospector (vice Harvester) level, we use one
+#   Prospector per path (Harvester) to allow for greater granularity & flexibility further down
+#   the ELK data pipeline.
+#
+#   In Hiera, specify filebeat_prospectors as a (primary) hash of (Prospector) hashes. We merge
+#   the hashes from the entire hierarchy, then create a Prospector per nested hash. At a minimum,
+#   each Prospector hash should specify "paths" (array) and "fields" (hash).
+#   (** see example Hiera at end of comments **)
 #
 #   Globs "*" are supported in "paths", but all files matching the glob will be treated the same
 #   by Filebeat -> Logstash -> Elasticsearch -> Kibana. 
@@ -35,17 +38,36 @@
 #   is an array of tags. Any other keys:values in the "fields" hash will become searchable fields
 #   in Elasticsearch. This allows you to add custom metadata for all events from a given log file.
 #
+#Exampe Hiera...
+##cirrus_logstash::client::filebeat_prospectors:
+##  var_log_auth_log:
+##    paths:
+##      - /var/log/auth.log
+##    fields:
+##      tags:
+##        - security
+##        - auth
+##        - syslog
+##      my_custom_field: "testing filebeat's custom fields"
+##  var_log_syslog:
+##   paths:
+##      - /var/log/syslog
+##    fields:
+##      tags:
+##        - syslog
+##      my_other_custom_field: "another test of filebeat's custom fields"
+#
+
 
 class cirrus_logstash::client (
-  $filebeat_prospectors = [],
-  $filebeat_enabled   = false,
-  $logstash_hosts     = ["${cirrus_site_iteration}-logstash-001.${domain}","${cirrus_site_iteration}-logstash-002.${domain}"],
-  $prospectors_system = hiera_array('cirrus_logstash::client::system_log_paths', ['/var/log/auth.log', '/var/log/syslog'] ),
+  $filebeat_enabled     = false,
+  $filebeat_prospectors = hiera_hash('filebeat_prospectors', {}),
+  $logstash_hosts       = ["${cirrus_site_iteration}-logstash-001.${domain}","${cirrus_site_iteration}-logstash-002.${domain}"],
 )
 {
   if $filebeat_enabled {
     class { 'filebeat':
-      logging     => {
+      logging           => {
         'level'     => 'info',
         'to_files'  => true,
         'to_syslog' => false,
@@ -56,8 +78,8 @@ class cirrus_logstash::client (
         },
       },
       #Set manage_repo to 'false' as we mirror the 'beats' repo with blobmaster & blobmirror.
-      manage_repo => false,
-      outputs     => {
+      manage_repo       => false,
+      outputs           => {
         'logstash' => {
           'hosts'       => $logstash_hosts,
           'loadbalance' => true,
@@ -84,7 +106,7 @@ class cirrus_logstash::client (
     }
     #Set $filebeat_prospectors to an array of hashes in Hiera.
     #Each hash is used as input to the filebeat::prospector define.
-    validate_array($filebeat_prospectors)
+    validate_hash($filebeat_prospectors)
     create_resources('filebeat::prospector', $filebeat_prospectors, $filebeat_prospectors_defaults)
     ###
 
